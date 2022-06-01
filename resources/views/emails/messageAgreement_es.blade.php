6a5dea5be80a95ca6970bd84a8e67486a06ac282
Giovanny Canasto <giovannyc28@gmail.com>
2022-06-01 00:12:31 -0500

    ajustando palntilla de correo

---
+++ resources/views/emails/messageAgreement_es.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <title></title>
+</head>
+<body>
+ <p>Estimado(a)Afiliado(a):<br>
+   <br>
+   Le damos una cordial bienvenida al Plan de Previsión Funerario Internacional, por favor revise la afiliación adjunta al email, en caso de cualquier pregunta o duda puede llamarnos al Departamento de Atención al Cliente (877)226-3006 y un asesor dará respuesta a todas sus inquietudes.<br>
+   <br>
+   Agradecemos su confianza al elegirnos, estamos para servirle a usted y a toda su familia los 365 días del año.<br>
+   <br>
+   Bienvenido a NASER<br>
+   <br>
+   <br>
+   <br>
+   Saludos,
+ </p>
+ 
+</body>
+</html> 
